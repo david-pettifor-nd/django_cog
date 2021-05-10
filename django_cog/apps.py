af1e4ed6b57036d0e997fa00894de378cb750ab3
David Pettifor <dpettifo@nd.edu>
2021-05-10 08:25:11 -0400

    put all code for cog registration in try/except incase migrations haven't had a chance to run yet

--- django_cog/apps.py
+++ django_cog/apps.py
@@ -7,17 +7,17 @@ def create_cog_records():
     registered cogs and creates Cog objects
     for them.
     """
-    from .models import Cog
-    from . import cog
-    active_cogs = []
-    for cog_name, function in cog.all.items():
-        Cog.objects.get_or_create(
-            name=cog_name
-        )
-        active_cogs.append(cog_name)
-
-    # remove any that are no longer registered
     try:
+        from .models import Cog
+        from . import cog
+        active_cogs = []
+        for cog_name, function in cog.all.items():
+            Cog.objects.get_or_create(
+                name=cog_name
+            )
+            active_cogs.append(cog_name)
+
+        # remove any that are no longer registered
         Cog.objects.all().exclude(name__in=active_cogs).delete()
     except Exception as e:
         print(e)
